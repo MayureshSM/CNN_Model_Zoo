24bff6855e811dbdd9a3ad3cd3bb44c29953f578
MAYURESHSM <mayuesh.maradkar87@gmail.com>
2022-10-16 12:16:14 +0530

    added Imagenet_classes and Viz

---
+++ Viz.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+class GUI(tk.TK):
+    def __init__(self):
+        super.__init__()
+        
---
+++ models.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+from torchvision.models import resnet50, alexnet, densenet121,googlenet,inception_v3,mobilenet_v3_small
+from torchvision import transforms
+from torchvision.io import read_image
+import numpy as np
+
+f=open('Imagenet_class.txt')
+classes=f.read().split('\n')
+
+class Object_classification():
+    def __init__(self):
+        self.transform=transforms.Compose([
+                                                                    transforms.ToTensor(),
+                                                                    transforms.Resize(232),
+                                                                    transforms.CenterCrop(224)
+                                                                    ])
+    def _modelrun(self,img,model):
+        model.eval()
+        img=self.transform(img)
+        res=model(img.unsqueeze(0))
+        print(classes[np.argsort(np.array(res.detach()[0])).astype(int)[-1] ])
+    
+    
+    def resnet50(self,img): 
+        model=resnet50(pretrained=True, progress=True)
+        self._modelrun(img,model)
+
+
+    def alexnet(self,img):
+        model = alexnet(pretrained=True)
+        self._modelrun(img,model)
+        
+    
+    def densenet121(self,img):
+        model=densenet121(pretrained=True)   
+        self._modelrun(img,model)
+        
+    
+    def googlenet(self,img):
+        model=googlenet(pretrained=True)   
+        self._modelrun(img,model)        
+                                
+    def inception(self,image):
+        model=inception_v3(pretrained=True)
+        self._modelrun(img,model)
+        
+    def mobilenet(self,img):
+        model=mobilenet_v3_small(pretrained=True)
+        self._modelrun(img,model)
+        
+
+img=plt.imread("/storage/emulated/0/Documents/Repos/Images/images5.jpg")
+A=Object_classification()
+A.mobilenet(img)      
